01f5574b399e0cd8e4ae3aa718fb7adb33b3acf5
Ruan da Fonseca Ramos <ruan.ramos28@gmail.com>
2023-11-26 01:38:05 -0300

    Add a utility script

---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Utilities
+    {
+        public static GameObject FindChildGameObjectByName(Transform parent, string name)
+        {
+            for (var i = 0; i < parent.childCount; i++)
+            {
+                if (parent.GetChild(i).name == name)
+                {
+                    return parent.GetChild(i).gameObject;
+                }
+
+                var child = FindChildGameObjectByName(parent.GetChild(i), name);
+                if (child != null)
+                {
+                    return child;
+                }
+            }
+
+            return null;
+        }
+    }
+}
